1fa75c28736ac7e4e2189e8463ef52f1ede2840c
Emma VANDECASTEELE <etu40268@henallux.be>
2021-01-10 21:48:19 +0100

    refactoring HomeFragment

--- app/src/main/java/com/smartcity/petsservices/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/smartcity/petsservices/ui/fragment/HomeFragment.kt
@@ -6,14 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.smartcity.petsservices.R
 import com.smartcity.petsservices.databinding.FragmentHomeBinding
-import com.smartcity.petsservices.model.Token
-import com.smartcity.petsservices.ui.viewModel.ProfileViewModel
-import java.util.*
 
 class HomeFragment : Fragment(){
 
@@ -26,8 +21,6 @@ class HomeFragment : Fragment(){
         binding.lifecycleOwner = this
 
         sharedPref = requireActivity().getSharedPreferences(getString(R.string.sharedPref), Context.MODE_PRIVATE);
-
-
 
         return binding.root;
     }
